011254595aa8d0d7a484e4075f389694003faee4
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2021-06-09 15:12:29 +0300

    09.06

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const header = document.querySelector('.wide-container');
+const nav = document.querySelector('.nav');
+const burger = document.querySelector('.burger');
+const hamburger = document.querySelector('.hamburger');
+const overlay = document.querySelector('.burger-overlay');
+const navLink = document.querySelector('.nav-link');
+
+const burgerMenu = function () {
+  nav.classList.toggle('burger-nav');
+  hamburger.classList.toggle('is-active');
+  overlay.classList.toggle('overlay-visible');
+  document.body.classList.toggle('lock');
+}
+const removeBurgerMenu = function () {
+  nav.classList.remove('burger-nav');
+  hamburger.classList.remove('is-active');
+  overlay.classList.remove('overlay-visible');
+  document.body.classList.remove('lock');
+}
+document.querySelector('.burger').addEventListener('click', burgerMenu);
+document.querySelector('.burger-overlay').addEventListener('click', burgerMenu);
+document.querySelector('.nav-link').addEventListener('click', burgerMenu);
