6b00e4dfb5354b7023ab65e769c5178526518b20
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2021-07-02 12:04:04 +0300

    02,07

--- js/script.js
+++ js/script.js
@@ -20,3 +20,28 @@ const removeBurgerMenu = function () {
 document.querySelector('.burger').addEventListener('click', burgerMenu);
 document.querySelector('.burger-overlay').addEventListener('click', burgerMenu);
 document.querySelector('.nav-link').addEventListener('click', burgerMenu);
+
+$('.responsive').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
