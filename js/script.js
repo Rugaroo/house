4a283ae28dfc0ee2610176f0c0d456b9ce424c85
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2021-07-05 21:39:00 +0300

    05.07

--- js/script.js
+++ js/script.js
@@ -96,3 +96,28 @@ $('.c-carousel').slick({
     }
   ]
 });
+
+var modal = document.getElementById('myModal');
+
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
